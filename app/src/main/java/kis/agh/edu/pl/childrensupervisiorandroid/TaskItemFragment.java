2fe1909da6165262da62fe2996b2417116a16f9c
Adam Swiezowski <adam.swiezowski+git@gmail.com>
2016-05-23 00:16:36 +0200

    add menu to appbar

--- app/src/main/java/kis/agh/edu/pl/childrensupervisiorandroid/TaskItemFragment.java
+++ app/src/main/java/kis/agh/edu/pl/childrensupervisiorandroid/TaskItemFragment.java
@@ -3,11 +3,15 @@ package kis.agh.edu.pl.childrensupervisiorandroid;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class TaskItemFragment extends Fragment {
 
+    private MainActivity mainActivity;
 
     public static TaskItemFragment newInstance() {
         TaskItemFragment fragment = new TaskItemFragment();
@@ -17,14 +21,32 @@ public class TaskItemFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.task_description_layout, container, false);
-
+        mainActivity=((MainActivity) getActivity());
+        setHasOptionsMenu(true);
         return view;
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        ((MainActivity) getActivity()).getSupportActionBar().setTitle("Task summary");
+        mainActivity.getSupportActionBar().setTitle("Task summary");
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.task_description_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.addPhotoAction:
+                return true;
+        }
+
+        return false;
     }
 
 }
